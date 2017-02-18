bdf0c01ec3db5434a399ffbe783f678544eb6ad3
Chad Gorshing <gorshing@gmail.com>
2017-02-18 09:47:44 -0600

    Increased buffer size

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 
 #include "thread_serve.h"
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 32768
 #define OK_IMAGE    "HTTP/1.0 200 OK\nContent-Type:image/gif\n\n"
 #define OK_TEXT     "HTTP/1.0 200 OK\nContent-Type:text/html\n\n"
 #define NOTOK_404   "HTTP/1.0 404 Not Found\nContent-Type:text/html\n\n"
