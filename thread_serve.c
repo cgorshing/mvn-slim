cba6e7e77b1b2ed2cd5909921a98a7fbe9f6e694
Chad Gorshing <gorshing@gmail.com>
2017-02-18 10:11:12 -0600

    Removing sjf scheduling

--- thread_serve.c
+++ thread_serve.c
@@ -144,88 +144,34 @@ void *thread_serve()
 //scheduler thread
 void *thread_scheduler(void *arg)
 {
-  unsigned int schedalg=*((unsigned int*)arg);
   int acceptfd,n;
-  if(schedalg==0)
-  {  
-    while(1)
-    {  
-      if(front!=NULL)
-      {  
-        sem_wait(sem);    
-        //printf("\nin sched thread before extracting element\n");
-        //printf("\nscheduler locking mutex\n");    
-        pthread_mutex_lock(&sthread_mutex);
-        pthread_mutex_lock(&qmutex);
-        r2=extract_element();
-        //printf("\n popped element in scheduler thread");
-        pthread_mutex_unlock(&qmutex);
-        
-        //printf("\nscheduler unlocked mutex\n");
-        // call serving thread from thread pool
-        
-        //printf("\nin sched thread before sending to serving thread\n");
-        
-        pthread_cond_signal(&cond_var);
-        
-        
-        free_thread--;
-        pthread_mutex_unlock(&sthread_mutex);        
-        //printf("\nin sched thread unlocked sthread mutex\n");
-        
-        //thread_serve(&r2);
-        //pthread_create(&t_serve,NULL,&thread_serve,&r);  
-        //thread_serve(&r);
-      }
-      else 
-      {
-        continue;
-      }      
-    }
-  }
-  else
+
+  while(1)
   {
-  //code for SJF scheduling algorithm
-    //printf("\n entered SJF scheduling algorithm");
-    int shortestjob_fd=0;
-    int min;
-    int a,b;
-    while(1)      
-    {  pthread_mutex_lock(&qmutex);
-      temp=front;
-      if (temp==NULL)
-      {
-        continue;
-      }
-      else if(temp->link==NULL) 
-      {
-        //printf("\n only one");
-        shortestjob_fd=temp->r.acceptfd;
-      }
-      else
-      {
-        min=temp->r.size;
-        while(temp->link!=NULL)  //should modify
-        {
-          b=temp->link->r.size;
-          if(min<=b)
-          {
-            shortestjob_fd=temp->r.acceptfd;
-          }    
-          else if(min>b)
-          {      
-            min=temp->link->r.size;            
-            shortestjob_fd=temp->link->r.acceptfd;
-          }
-          printf("\n %d",a);
-          temp=temp->link;
-        }
-      }
+    if(front!=NULL)
+    {
+      log_msg("Going into sem_wait");
+
+      //Wait for an available thread
+      sem_wait(sem);
+      log_msg("in sched thread before extracting element");
+      log_msg("scheduler locking mutex");
       pthread_mutex_lock(&sthread_mutex);
-      pthread_cond_signal(&cond_var);
-      pthread_mutex_unlock(&sthread_mutex);
-            
+
+      log_msg("Extracting next item from list...");
+      pthread_mutex_lock(&qmutex);
+      r2 = extract_element();
       pthread_mutex_unlock(&qmutex);
+      log_msg("Done Extracting next item from list.");
+
+      // call serving thread from thread pool
+
+      log_msg("in sched thread before sending to serving thread");
+
+      pthread_cond_signal(&cond_var);
+
+      pthread_mutex_unlock(&sthread_mutex);
+      log_msg("in sched thread unlocked sthread mutex");
     }
   }
 }
